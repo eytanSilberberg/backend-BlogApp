206a6be2f51d4da68fe817595a87e194006d412e
Eytan Silberberg <102313688+eytanSilberberg@users.noreply.github.com>
2023-08-10 16:29:01 +0300

    addeddev enviroment and changed collection in database to dev collection

--- routes/post.py
+++ routes/post.py
@@ -12,9 +12,10 @@ router = APIRouter()
 async def create_post(post: PostToSave, db=Depends(get_database)):
     try:
         # Create a new post in the database
-        inserted_post = await db.post.insert_one(post.dict())
+        inserted_post = await db.post_dev.insert_one(post.dict())
+        print('lllll')
         post_id = inserted_post.inserted_id
-        post = await db.post.find_one({"_id": post_id})
+        post = await db.post_dev.find_one({"_id": post_id})
         modified_post = {"id": str(post.pop("_id")), **post}
         print(modified_post)
     except Exception as e:
@@ -28,7 +29,7 @@ async def read_post(post_id: str, db=Depends(get_database)):
     # Retrieve a post from the database by ID
     print(post_id)
     postId = ObjectId(post_id)
-    post = await db.post.find_one({"_id": postId})
+    post = await db.post_dev.find_one({"_id": postId})
     modified_post = {"id": str(post.pop("_id")), **post}
     print(post)
     if post is None:
@@ -42,7 +43,7 @@ async def update_post(post_id: str, post: Post, db=Depends(get_database)):
     postId = ObjectId(post_id)
     print(postId)
     print("post", post)
-    existing_post = await db.post.find_one({"_id": postId})
+    existing_post = await db.post_dev.find_one({"_id": postId})
     print("existing_post", existing_post)
     # del post[id]
     if existing_post is None:
@@ -51,8 +52,8 @@ async def update_post(post_id: str, post: Post, db=Depends(get_database)):
     # Update the post in the database
     replika_post = {"title": post.title,
                     "description": post.description, "image": post.image}
-    await db.post.update_one({"_id": postId}, {"$set": replika_post})
-    updated_post = await db.post.find_one({"_id": postId})
+    await db.post_dev.update_one({"_id": postId}, {"$set": replika_post})
+    updated_post = await db.post_dev.find_one({"_id": postId})
     print(updated_post)
     modified_post = {"id": str(updated_post.pop("_id")), **updated_post}
 
@@ -63,13 +64,13 @@ async def update_post(post_id: str, post: Post, db=Depends(get_database)):
 async def delete_post(post_id: str, db=Depends(get_database)):
     # Check if the post exists
     postId = ObjectId(post_id)
-    existing_post = await db.post.find_one({"_id": postId})
+    existing_post = await db.post_dev.find_one({"_id": postId})
     print(existing_post)
     if existing_post is None:
         raise HTTPException(status_code=404, detail="Post not found")
 
     # Delete the post from the database
-    res = await db.post.delete_one({"_id": postId})
+    res = await db.post_dev.delete_one({"_id": postId})
     print(res.deleted_count)
     modified_post = {"id": str(existing_post.pop("_id")), **existing_post}
     return PostToDeleteResponse(**{"data": modified_post, "status": "success"})
@@ -79,7 +80,7 @@ async def delete_post(post_id: str, db=Depends(get_database)):
 async def read_posts(db=Depends(get_database)):
     try:
         # Retrieve all posts from the database
-        posts = await db.post.find().to_list(length=None)
+        posts = await db.post_dev.find().to_list(length=None)
         modified_posts = [{"id": str(post.pop("_id")), **post}
                           for post in posts]
         res = {
